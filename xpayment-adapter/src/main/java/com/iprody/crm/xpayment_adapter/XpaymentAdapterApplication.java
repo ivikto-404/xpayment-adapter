ca6dd1f0fc129979a613eaeadcac1aa19abb2a82
ivikto <i.zhabchenko@yandex.ru>
2025-05-06 13:59:01 +0300

    Initial commit

---
+++ xpayment-adapter/src/main/java/com/iprody/crm/xpayment_adapter/XpaymentAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.iprody.crm.xpayment_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpaymentAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpaymentAdapterApplication.class, args);
+	}
+
+}
---
+++ xpayment-adapter/src/test/java/com/iprody/crm/xpayment_adapter/XpaymentAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iprody.crm.xpayment_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpaymentAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
